310a4344326ecf13f16103ad5653f5cd98468e76
Yizhuo Wu <yizhuw1@uci.edu>
2021-03-16 16:18:41 +0800

    Uploaded Lambdas

---
+++ index-photos/lambda_function.py
@@ -0,0 +1,80 @@
+import boto3
+import os
+import sys
+import uuid
+from urllib.parse import unquote_plus
+import time
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import requests
+
+def upload_to_elasticsearch(jsonDocu):
+
+    host = 'search-photos-fntfv7yrgxc4zptmna5agsqkqe.us-west-2.es.amazonaws.com'
+    es = Elasticsearch(
+        hosts = [{'host': host, 'port': 443}],
+        http_auth = ("AdminMaster", "Admin@98"),
+        use_ssl = True,
+        verify_certs = True,
+        connection_class = RequestsHttpConnection
+    )
+    
+
+    es.index(index="photos", doc_type="_doc", id=jsonDocu["objectKey"], body=jsonDocu)
+    
+    print(es.get(index="photos", doc_type="_doc", id=jsonDocu["objectKey"]))
+    
+
+
+def lambda_handler(event, context):
+    print("uploaded photo")
+    print(event)
+    
+    s3_client = boto3.client('s3')
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        response = s3_client.head_object(
+            Bucket=bucket,
+            Key=key
+        )
+        print(response)
+        print(response["Metadata"])
+        
+    
+    pass
+    
+    '''
+    s3_client = boto3.client('s3')
+    reko_client = boto3.client('rekognition')
+    
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = unquote_plus(record['s3']['object']['key'])
+        print("here")
+
+        response = reko_client.detect_labels(
+            Image={
+                'S3Object': {
+                    'Bucket': bucket,
+                    'Name': key
+                }
+            },
+            MaxLabels=10
+        )
+        
+        labeldict = {}
+        labeldict["objectKey"] = key
+        labeldict["bucket"] = bucket
+        labeldict["createdTimestamp"] = local_time = time.ctime(time.time())
+        labeldict["labels"] = []
+        
+        for item in response["Labels"]:
+            labeldict["labels"].append(item["Name"])
+            
+        print(str(labeldict))
+        
+        upload_to_elasticsearch(labeldict)
+    '''
+
+        
---
+++ search-photos/lambda_function.py
@@ -0,0 +1,81 @@
+import json
+import requests
+import boto3
+
+def search(item, allurl):
+    headers = { "Content-Type": "application/json" }
+    ENDPOINT = "https://search-photos-fntfv7yrgxc4zptmna5agsqkqe.us-west-2.es.amazonaws.com"
+    url = ENDPOINT + '/_search?q=' + item
+    response = requests.get(url, headers=headers, auth=("AdminMaster", "Admin@98")).json()
+    print(response["hits"]["hits"])
+    
+    for pic in response["hits"]["hits"]:
+        allurl.append("https://photostorage-hw2.s3.amazonaws.com/" + pic["_id"])
+    
+    
+def connectwithlex(userinput):
+    lex_client = boto3.client('lex-runtime') 
+    
+    response = lex_client.post_text(
+        botName='PhotoSearch',
+        botAlias='Final',
+        userId='test',
+        inputText=userinput
+    )
+    try:
+        print(response)
+        slots = response['slots']
+        print(slots)
+        if (slots["AnswerTwo"] == None):
+            lex_client.post_text(
+                botName='PhotoSearch',
+                botAlias='Final',
+                userId='test',
+                inputText="cat"
+            )
+            return [slots['Answer']]
+        else:
+            return[slots['Answer'], slots['AnswerTwo']]
+    except:
+        return []
+ 
+def lambda_handler(event, context):
+    # TODO implement
+    
+    try:
+        print(event["queryStringParameters"]['q'])
+        userinput = event["queryStringParameters"]['q']
+        
+        searchitem = connectwithlex(userinput)
+        allurl = []
+        for item in searchitem:
+            search(item, allurl)
+    
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "headers": {"Access-Control-Allow-Origin" : "*","Access-Control-Allow-Credentials" : True},
+            'body': json.dumps({
+                'images':allurl}
+                )
+        }
+    except:
+        return {
+            "isBase64Encoded": False,
+            "statusCode": 200,
+            "headers": {"Access-Control-Allow-Origin" : "*","Access-Control-Allow-Credentials" : True},
+            'body': json.dumps({
+                'images':[]}
+                )
+        }       
+
+    '''
+    searchitem = connectwithlex()
+    
+    allurl = []
+    for item in searchitem:
+        search(item, allurl)
+        
+    print(allurl)
+    '''
+   
